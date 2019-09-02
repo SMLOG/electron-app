f7f06f3142702dbcab49e3db8771522a73235cba
alex <alex@10.82.145.130>
2019-09-02 17:49:30 +0800

    update

--- static/preload.js
+++ static/preload.js
@@ -2,17 +2,28 @@ let electron = require("electron");
 let win = electron.remote.getCurrentWindow();
 let timerID;
 
+let wsize = win.getSize();
 document.addEventListener("mouseleave", event => {
-  timerID = setTimeout(() => {
-    window.close();
-  }, 1000);
+
+  timerID = setInterval(() => {
+    let mousePos = electron.screen.getCursorScreenPoint();
+    let wPos = win.getPosition();
+
+
+    if (mousePos.x < wPos[0] || mousePos.x > wPos[0] + wsize[0] || mousePos.y < wPos[1] || mousePos.y > wPos[1] + wsize[1]) {
+      window.close();
+    }
+    //alert(mousePos.x)
+
+
+  }, 500);
   //console.log(event);
 });
 document.addEventListener("mouseenter", event => {
   if (timerID) clearTimeout(timerID);
   //console.log(event);
 });
-document.addEventListener("mousedown", function(e) {
+document.addEventListener("mousedown", function (e) {
   switch (e.button) {
     case 0:
       biasX = e.x;
@@ -25,7 +36,7 @@ document.addEventListener("mousedown", function(e) {
   }
 });
 
-document.addEventListener("mouseup", function() {
+document.addEventListener("mouseup", function () {
   biasX = 0;
   biasY = 0;
   document.removeEventListener("mousemove", moveEvent);

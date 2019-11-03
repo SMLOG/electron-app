818261cf19e889abc4fc20323b2244d28fe9e1e4
yourname <youremail>
2019-11-03 21:40:13 +0800

    update

--- src/renderer/view/home.vue
+++ src/renderer/view/home.vue
@@ -1,99 +1,97 @@
 <template>
   <div>
     <iframe src="static/tech.html?sh000001" style="width:100%;height:600px;display:none;"></iframe>
-    <div
-      id="top"
-      style="
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0%;
-    overflow: auto;
-    position: absolute;
-    overflow-x:hidden;
-"
-      ref="top"
-    >
+    <div id="top" ref="top">
       <div>
-        <div style="float:left;">
-          <ul class="filters">
-            <li v-for="(k,filter) in filters" :key="filter">
-              <a
-                @click="visibility=filter"
-                :class="{ selected: visibility == filter }"
-              >{{filter}}({{filterCounts[filter]}})</a>
-            </li>
-          </ul>
+        <div>
+          <div style="float:left;">
+            <ul class="filters">
+              <li v-for="(k,filter) in filters" :key="filter">
+                <a
+                  @click="visibility=filter"
+                  :class="{ selected: visibility == filter }"
+                >{{filter}}({{filterCounts[filter]}})</a>
+              </li>
+            </ul>
+          </div>
+          <div style="float:right;">
+            <search-panel @select="addItem"></search-panel>
+          </div>
         </div>
-        <div style="float:right;">
-          <search-panel @select="addItem"></search-panel>
-        </div>
-      </div>
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th
-                v-for="col in head"
-                @click="sort(col.prop)"
-                :key="col.prop"
-                :class="{ 
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th
+                  v-for="col in head"
+                  @click="sort(col.prop)"
+                  :key="col.prop"
+                  :class="{ 
                     ascending : sortby === col.prop && !descending,
                     descending: sortby ===col.prop && descending
                 }"
-              >{{col.label}}</th>
-              <th>A</th>
-            </tr>
-            <tr v-if="selectItem &&  selectItem.tables&&selectItem.tables.length>0">
-              <th :colspan="head.length+2">
-                <div id="detail" ref="detail">
-                  <span v-if="selectItem.tables&&selectItem.tables.length>0">
-                    <div v-for="t in selectItem.tables" :key="t.str">
-                      {{selectItem.name}}
-                      <span v-html="t.str"></span>
-                    </div>
-                  </span>
-                </div>
-              </th>
-            </tr>
-          </thead>
-          <draggable v-model="items" @update="dragEnd" tag="tbody">
-            <tr
-              class="item"
-              v-for="(item,index) in filteredItems"
-              :key="item.code"
-              :class="{'odd':index%2 != 1}"
-            >
-              <td :title="item.code" :class="{lk:item.tables&&item.tables.length>0}">
-                <span :class="{sz:item.mk=='sz'}" @click="openlink(item,$event)">{{item.name}}</span>
-                <span
-                  @click="toggleDetail(item)"
-                  :class="{avggood:item.avgzs>45 && item.upArgCount>120}"
-                >{{item.avgzs}}/{{item.upArgCount}}</span>
-                <span v-if="item.contDir!=0">/{{item.contDir}}</span>
-              </td>
+                >{{col.label}}</th>
+                <th>A</th>
+              </tr>
+              <tr v-if="selectItem &&  selectItem.tables&&selectItem.tables.length>0">
+                <th :colspan="head.length+2">
+                  <div id="detail" ref="detail">
+                    <span v-if="selectItem.tables&&selectItem.tables.length>0">
+                      <div v-for="t in selectItem.tables" :key="t.str">
+                        {{selectItem.name}}
+                        <span v-html="t.str"></span>
+                      </div>
+                    </span>
+                  </div>
+                </th>
+              </tr>
+            </thead>
+            <draggable v-model="items" @update="dragEnd" tag="tbody">
+              <tr
+                class="item"
+                v-for="(item,index) in filteredItems"
+                :key="item.code"
+                :class="{'odd':index%2 != 1}"
+              >
+                <td :title="item.code" :class="{lk:item.tables&&item.tables.length>0}">
+                  <span :class="{sz:item.mk=='sz'}" @click="openlink(item,$event)">{{item.name}}</span>
+                  <span
+                    @click="toggleDetail(item)"
+                    :class="{avggood:item.avgzs>45 && item.upArgCount>120}"
+                  >{{item.avgzs}}/{{item.upArgCount}}</span>
+                  <span v-if="item.contDir!=0">/{{item.contDir}}</span>
+                </td>
 
-              <td
-                v-for="col in head"
-                :key="col.prop"
-                :class="col.class&&col.class(item)"
-              >{{col.fmt?col.fmt(item[col.prop],item):item[col.prop]}}</td>
+                <td
+                  v-for="col in head"
+                  :key="col.prop"
+                  :class="col.class&&col.class(item)"
+                >{{col.fmt?col.fmt(item[col.prop],item):item[col.prop]}}</td>
 
-              <td>
-                <input type="checkbox" v-model="item.isFocus" @change="saveDatas(item)" />
-                <a style="float:right;" class="action" @click="delItem(item)">x</a>
-              </td>
-            </tr>
-          </draggable>
-        </table>
+                <td>
+                  <input type="checkbox" v-model="item.isFocus" @change="saveDatas(item)" />
+                  <a style="float:right;" class="action" @click="delItem(item)">x</a>
+                </td>
+              </tr>
+            </draggable>
+          </table>
+        </div>
       </div>
     </div>
-    <webview
-      ref="webview"
-      id="figure"
-      style="position:absolute;left:0;right:0;bottom:0;top:100%;display:none;z-index:100"
-    ></webview>
+    <div
+      ref="webviewWrap"
+      style="position:fixed;left:0;right:0;bottom:0;top:60%;display:none;z-index:100"
+    >
+      <div
+        id="dragBar"
+        ref="dragBar"
+        style="position:absolute;width:100%;height:10px;cursor:row-resize;"
+        v-drag
+        draggable="false"
+      ></div>
+      <webview ref="webview" id="figure" style="width:100%;height:100%;"></webview>
+    </div>
   </div>
 </template>
 
@@ -111,7 +109,9 @@ import {
   fetchEval,
   dateFormat,
   timeout,
-  openKlineWindow
+  openKlineWindow,
+  setCookie,
+  getCookie
 } from "@/lib/utils";
 import { headers } from "./headers";
 import { monitor } from "@/lib/monitor";
@@ -131,6 +131,37 @@ export default {
       items2: [],
       filterCounts: {}
     };
+  },
+  directives: {
+    drag(el) {
+      let oDiv = $(el).parent()[0]; //当前元素
+      let self = this; //上下文
+      //禁止选择网页上的文字
+      document.onselectstart = function() {
+        return false;
+      };
+      el.onmousedown = function(e) {
+        //鼠标按下，计算当前元素距离可视区的距离
+        let disX = e.clientX - oDiv.offsetLeft;
+        let disY = e.clientY - oDiv.offsetTop;
+        document.onmousemove = function(e) {
+          //通过事件委托，计算移动的距离
+          let l = e.clientX - disX;
+          let t = e.clientY - disY;
+          //移动当前元素
+          // target.style.left = l + "px";
+          oDiv.style.top = t + "px";
+        };
+        document.onmouseup = function(e) {
+          document.onmousemove = null;
+          document.onmouseup = null;
+          $("#top").css("margin-bottom", $(oDiv).outerHeight());
+          setCookie("charTop", $(oDiv).outerHeight());
+        };
+        //return false不加的话可能导致黏连，就是拖到一个地方时div粘在鼠标上不下来，相当于onmouseup失效
+        return false;
+      };
+    }
   },
   components: {
     SearchPanel,
@@ -173,17 +204,26 @@ export default {
   methods: {
     openlink(item, event) {
       let webview = $(this.$refs.webview);
+      let webviewWrap = $(this.$refs.webviewWrap);
       let td = $(event.target).closest("td");
 
       let url = `http://localhost:9080/static/tech.html?${item.code}`;
-      if (webview[0].src.indexOf(url) > -1 && webview.is(":visible")) {
-        webview.hide();
-        $(this.$refs.top).css("bottom", "0%");
-      } else {
-        webview.show();
-        $(this.$refs.top).css("bottom", "40%");
 
-        webview.css("top", "60%");
+      if (webview[0].src.indexOf(url) > -1 && webviewWrap.is(":visible")) {
+        webviewWrap.hide();
+        $(this.$refs.top).css("margin-bottom", "0");
+      } else {
+        if (!webviewWrap.is(":visible")) {
+          let chartop = getCookie("charTop", $(window).height() * 0.6);
+          console.log(chartop, $(window).height() * 0.6);
+          $(this.$refs.top).css(
+            "margin-bottom",
+            $(window).height() - chartop + "px"
+          );
+          webviewWrap.css("top", chartop + "px");
+        }
+        webview[0].style.height = "100%";
+        webviewWrap.show();
       }
       webview.attr("src", url);
 

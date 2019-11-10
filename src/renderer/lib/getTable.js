3f9cdb20692599346e9445cb4c6cc0dae14f27f4
yourname <youremail>
2019-11-10 15:09:34 +0800

    update

--- src/renderer/lib/getTable.js
+++ src/renderer/lib/getTable.js
@@ -6,7 +6,8 @@ import {
   split,
   parse,
   fetchEval,
-  awaitTimeout
+  awaitTimeout,
+  rid
 } from "./utils";
 import { getExcludeList } from "./exclude-list";
 import { getCache, putCache, getCacheData } from "./db";
@@ -386,7 +387,7 @@ function isCP(klines) {
   return ret > 2 && retp < 0.08;
 }
 export async function getFindList() {
-  let cb = "cb" + +new Date();
+  let cb = rid("list");
 
   let url = `http://25.push2.eastmoney.com/api/qt/clist/get?cb=${cb}&pn=1&pz=20000&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:13,m:0+t:80,m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f26,f27,f28,f29,f22,f11,f62,f128,f136,f115,f152&_=1572107420243`;
   let p = new Promise((resolve, reject) => {
--- src/renderer/lib/ind.js
+++ src/renderer/lib/ind.js
@@ -1,9 +1,9 @@
-import { fetchEval } from "./utils";
+import { fetchEval, rid } from "./utils";
 import { getCacheData, cache } from "./db";
 
 export async function getAllInd() {
   let resp = await getCacheData(null, "industry", async () => {
-    let cb = "cb" + +new Date();
+    let cb = rid("ind");
     let url = `http://25.push2.eastmoney.com/api/qt/clist/get?cb=${cb}&pn=1&pz=2000&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:90+t:2&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f26,f22,f33,f11,f62,f128,f136,f115,f152,f124,f107,f104,f105,f140,f141,f207,f222&_=1573131628403`;
 
     let p = new Promise((resolve, reject) => {
@@ -35,7 +35,7 @@ export async function getAllInd() {
         let it = respi.data.diff[k];
         let code = (it.f12.substring(0, 1) == "6" ? "sh" : "sz") + it.f12;
 
-        await getCacheData(null, "ind_" + code, function() {
+        await getCacheData(null, "ind_" + code, async function() {
           return item.f14;
         });
       }

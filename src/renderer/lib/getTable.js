cb669f799110d79e6d61d6da9c6f8d3bd65c4c4a
yourname <youremail>
2019-11-13 18:35:49 +0800

    update

--- src/renderer/lib/getTable.js
+++ src/renderer/lib/getTable.js
@@ -293,6 +293,60 @@ export async function updateItem(item) {
   return item;
 }
 
+const fieldMap = {
+  jbmgsy: "基本每股收益(元)",
+  kfmgsy: "扣非每股收益(元)",
+  xsmgsy: "稀释每股收益(元)",
+  mgjzc: "每股净资产(元)",
+  mggjj: "每股公积金(元)",
+  mgwfply: "每股未分配利润(元)",
+  mgjyxjl: "每股经营现金流(元)",
+  yyzsr: "营业总收入(元)",
+  mlr: "毛利润(元)",
+  gsjlr: "归属净利润(元)",
+  kfjlr: "扣非净利润(元)",
+  yyzsrtbzz: "营业总收入同比增长(%)",
+  gsjlrtbzz: "归属净利润同比增长(%)",
+  kfjlrtbzz: "扣非净利润同比增长(%)",
+  yyzsrgdhbzz: "营业总收入滚动环比增长(%)",
+  gsjlrgdhbzz: "归属净利润滚动环比增长(%)",
+  kfjlrgdhbzz: "扣非净利润滚动环比增长(%)",
+  jqjzcsyl: "加权净资产收益率(%)",
+  tbjzcsyl: "摊薄净资产收益率(%)",
+  tbzzcsyl: "摊薄总资产收益率(%)",
+  mll: "毛利率(%)",
+  jll: "净利率(%)",
+  sjsl: "实际税率(%)",
+  yskyysr: "预收款/营业收入",
+  xsxjlyysr: "销售现金流/营业收入",
+  jyxjlyysr: "经营现金流/营业收入",
+  zzczzy: "总资产周转率(次)",
+  yszkzzts: "应收账款周转天数(天)",
+  chzzts: "存货周转天数(天)",
+  zcfzl: "资产负债率(%)",
+  ldzczfz: "流动负债/总负债(%)",
+  ldbl: "流动比率",
+  sdbl: "速动比率"
+};
+function fmtReportDatas(json) {
+  let reportDate = [];
+  let map = {};
+  for (let it of json) {
+    for (let k in it) {
+      let key = fieldMap[k] || k;
+      if (!map[key]) map[key] = {};
+      map[key][it.date] = it[k];
+    }
+  }
+
+  for (let i in map["date"]) {
+    reportDate.push(i);
+  }
+  reportDate.sort().reverse();
+  reportDate.unshift("报告日期");
+  map["reportDate"] = reportDate;
+  return map;
+}
 export function attachData(item) {
   return (async () => {
     /*if (!getCache(`${item.code}_240`)) {
@@ -312,7 +366,7 @@ export function attachData(item) {
     for (let i = 0; i < tbls.length; i++) {
       let tbname = tbls[i];
       const tbVarName = "tb_" + tbname + item.code;
-      await getCacheData(null, tbVarName, async () => {
+      let data = await getCacheData(null, tbVarName, async () => {
         let blob = await fetch(
           `http://quotes.money.163.com/service/${tbname}_${item.code.replace(
             /[^0-9]/g,
@@ -330,7 +384,17 @@ export function attachData(item) {
           reader.readAsText(blob, "GBK");
         });
       });
+      window[tbVarName] = data;
     }
+    let zyzb = await getCacheData(null, "zyzb_" + item.code, async () => {
+      let code = item.code.replace("sz", "SZ").replace("sh", "SH");
+      let url = `http://f10.eastmoney.com/NewFinanceAnalysis/MainTargetAjax?type=0&code=${code}`;
+      let json = await fetch(url).then(res => res.json());
+      json = fmtReportDatas(json);
+      return json;
+    });
+    window["zyzb_" + item.code] = zyzb;
+
     await updateItem(item);
   })();
 }

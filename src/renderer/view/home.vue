cdb139ce7501fcca00d08dd8de3cbfc549e55075
Your Name <you@example.com>
2020-01-11 18:41:24 +0800

    update

--- src/renderer/view/home.vue
+++ src/renderer/view/home.vue
@@ -235,6 +235,7 @@ export default {
         //鼠标按下，计算当前元素距离可视区的距离
         let disX = e.clientX - oDiv.offsetLeft;
         let disY = e.clientY - oDiv.offsetTop;
+       let winH = $(window).outerHeight();
         document.onmousemove = function(e) {
           //通过事件委托，计算移动的距离
           let l = e.clientX - disX;
@@ -242,6 +243,7 @@ export default {
           //移动当前元素
           // target.style.left = l + "px";
           if(t<=0)t=0;
+          else if(t>=winH)t=winH-8;
           oDiv.style.top = t + "px";
           
         };

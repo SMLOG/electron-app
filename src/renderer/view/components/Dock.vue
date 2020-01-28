0a42ec97f88bdd0d7e8af461362f7ff840b8c079
yourname <youremail>
2020-01-28 21:58:30 +0800

    dock

--- src/renderer/view/components/Dock.vue
+++ src/renderer/view/components/Dock.vue
@@ -35,18 +35,20 @@ export default {
       let height = Math.max(
         body.scrollHeight,
         body.offsetHeight,
-        html.clientHeight,
-        html.scrollHeight,
-        html.offsetHeight
+        html.offsetHeight,
+        27
       );
-      console.log(height);
-      if (winSize[1] != height)
-        this.setSize(winSize[0], (win.isFrame ? 25 : 0) + height);
+      let con = this.$electron.remote.getGlobal("console");
+      con.log(body.scrollHeight, body.offsetHeight, html.offsetHeight, height);
+      if (winSize[1] != height) this.setSize(winSize[0], height);
     },
     setSize(w, h) {
       let win = this.$electron.remote.getCurrentWindow();
       //win.setResizable(true);
-      win.setSize(w, h);
+      win.setSize(w, (win.isFrame ? 25 : 0) + h);
+      let con = this.$electron.remote.getGlobal("console");
+      con.log(h);
+
       // win.setResizable(false);
     }
   },
@@ -59,8 +61,6 @@ export default {
 
     let wsize = win.getSize();
     document.addEventListener("mouseleave", event => {
-      console.log(win);
-
       timerID = setInterval(() => {
         let mpos = this.$electron.screen.getCursorScreenPoint();
         let wPos = win.getPosition();
@@ -80,16 +80,31 @@ export default {
           if (this.isShrink) {
             this.setSize(wsize[0], 1 * 27);
           }
+          timerID && clearInterval(timerID);
         }
       }, 500);
     });
     document.addEventListener("mouseenter", event => {
       if (timerID) clearTimeout(timerID);
       if (this.isDockLeft) {
-        win.setPosition(
+        var x = win.getPosition()[0];
+        let targetX =
+          screen.getPrimaryDisplay().workAreaSize.width - win.getSize()[0];
+        let k = 1;
+
+        function step() {
+          x = Math.max(x - 10 * k++, targetX);
+          win.setPosition(x, win.getPosition()[1]);
+          if (x > targetX) {
+            window.requestAnimationFrame(step);
+          }
+        }
+        step();
+
+        /* win.setPosition(
           screen.getPrimaryDisplay().workAreaSize.width - win.getSize()[0],
           win.getPosition()[1]
-        );
+        );*/
       }
       if (this.isShrink) {
         this.resizeWin();

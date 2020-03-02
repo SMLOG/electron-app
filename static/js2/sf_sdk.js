227a4c46df40490cd67d0d73d84d6255c44bc473
yourname <youremail>
2020-03-02 13:33:51 +0800

    update rate to 1 seconds

--- static/js2/sf_sdk.js
+++ static/js2/sf_sdk.js
@@ -3702,7 +3702,7 @@ xh5_define("cfgs.settinger", [], function() {
       }),
       (this.PARAM = {
         K_CL_NUM: 260,
-        updateRate: 5,
+        updateRate: 1,
         T_RATE: 120,
         minCandleNum: 25,
         maxCandleNum: 0 / 0,

ac628255b19e05be9094c6d3e9bb69bc0c2d2950
alex <alex@youremail>
2020-08-02 16:01:54 +0800

    clean6

---
+++ src/renderer/view/components/WinViewInline.vue
@@ -0,0 +1,136 @@
+<template>
+  <div id="webview" :src="link" style="background-color:white;">
+    <div @dblclick="dbclick" class="info" v-show="false">
+      <span>{{ item.hy }}</span>
+      <span v-if="item.forecast">{{ item.forecast }}</span>
+      <span>披露:{{ item.disclosure }}</span>
+      <span>流通/亿:{{ item["ltg"] }}</span>
+      <span>流/总:{{ item["lz"] }}</span>
+      <span>TTM:{{ item["pe_ttm"] }}</span>
+      <span>PEG:{{ item["PEG"] && item["PEG"].toFixed(2) }}</span>
+      <span>同比:{{ item["tbzz"] && item["tbzz"].toFixed(2) }}</span>
+      <span>换手率:{{ item["turnover"] }}%</span>
+      <span :class="{up:item.lb>1,down:item.lb<1}">量比:{{ item["lb"] }}</span>
+      <span>低:{{ item["low"] }}</span>
+      <span>高:{{ item["high"] }}</span>
+      <span>振幅:{{ item["zf"] }}%</span>
+      <span>成交额:{{(item.amount/100000000).toFixed(2)}}亿</span>
+      <span :title="item.score_desc">分数:{{item.score}}/{{item.tscore}}</span>
+    </div>
+    <div id="hqMain">
+      <section id="cn_hqdata" class="hq_hqdata" style="display: none;"></section>
+
+      <!-- 行情图组件 start -->
+      <section class="cn-chart">
+        <div class="cn-chart-box" id="h5Chart"></div>
+      </section>
+      <!-- 行情图组件 end -->
+    </div>
+    <div id="hq_chart"></div>
+
+    <!-- 实时行情套件 -->
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+import Calendar from "@/view/components/calendar";
+
+import store from "@/localdata";
+import draggable from "vuedraggable";
+import { filters, getCheckFilters, afilters } from "@/lib/filters";
+import { callFun } from "@/lib/tech-manager";
+import moment from "moment";
+
+import $ from "jquery";
+import { loadScripts } from "@/lib/utils";
+
+window.$ = $;
+
+export default {
+  name: "WinView",
+  data: function () {
+    return {
+      openCode: null,
+      openType: null,
+      filters: filters,
+    };
+  },
+  props: {
+    link: String,
+    item: Object,
+    dBclick: Function,
+  },
+  components: {
+    Calendar,
+  },
+  mounted() {
+    window.paperCode = "sz002048";
+    setTimeout(() => {
+      let loadscript = loadScripts([
+        "/static/js/sf_sdk.js",
+        "static/js2/IO.WebPush4.js",
+        "static/js2/IO.WebPush4.js",
+        "static/js2/HQ_DataApps.js?hsw=7.32",
+        "static/js2/zepto.min.js",
+        "static/js2/sf_sdk.js",
+        "static/js2/sinaAppTKChart.js",
+        "static/js2/cn2017.js?hsw=7.32",
+      ]);
+    }, 5000);
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    dbclick() {
+      this.$emit("dBclick");
+    },
+
+    closeview() {
+      let webviewWrap = $(this.$refs.webviewWrap);
+      webviewWrap.hide();
+      $(this.$refs.top).css("margin-bottom", "0");
+      this.openCode = null;
+    },
+  },
+};
+</script>
+<style scoped>
+.info span {
+  margin-right: 8px;
+  font-size: 80%;
+}
+
+#dragBar {
+  position: absolute;
+  width: 100%;
+  height: 5px;
+  cursor: row-resize;
+  text-align: center;
+  border-top: 1px solid #1bc07d;
+}
+.y {
+  color: red;
+}
+.button {
+  border: none;
+  cursor: pointer;
+  padding: 0px 12px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 12px;
+  border-bottom: 1px solid;
+}
+.gray {
+  color: gray;
+}
+.up {
+  color: red;
+}
+.down {
+  color: green;
+}
+</style>
+
+<style scoped src="./WinViewInline.css" />
